64e9c5de98a2a6c48b98d0920ce36b3cefe93e89
Ondrej Certik <ondrej@certik.cz>
2009-07-16 22:14:42 -0600

    committing my stuff

--- t.py
+++ t.py
@@ -31,6 +31,7 @@ def run_tests(tests, nums):
 
 
 from IPython.kernel import client
+tc = client.TaskClient()
 mec = client.MultiEngineClient()
 mec.reset()
 ids = mec.get_ids()
@@ -48,36 +49,62 @@ ntests = ntests[0]
 print "number of tests:", ntests
 mec.execute("from sympy.utilities.runtests import run_test_function")
 i = 0
-tasks = {}
+#tasks = {}
 #ntests = 20
-for testi in range(ntests):
-    if i not in tasks:
-        tasks[i] = []
-    tasks[i].append(testi)
-    i += 1
-    if not (i in ids):
-        i = 0
-    #print i, testi
-mec.push(dict(tasks=tasks))
+#for testi in range(ntests):
+#    if i not in tasks:
+#        tasks[i] = []
+#    tasks[i].append(testi)
+#    i += 1
+#    if not (i in ids):
+#        i = 0
+#    #print i, testi
+#mec.push(dict(tasks=tasks))
 results = []
-print "distributing jobs"
-for target in tasks:
-    pr = mec.execute("result = run_tests(tests, tasks[%d])" % target,
-            targets=[target], block=False)
-    results.append((target, pr))
-print "collecting results"
-for proc, r in results:
-    print "processor:", proc
-    r.get_result()
-    result = mec.pull("result", targets=proc)[0]
-    infos = []
-    for res, info in result:
-        if res == ".":
-            print ".",
-        else:
-            print "F",
-            infos.append(info)
-    print "exceptions"
-    for e in infos:
-        print "_"*80
-        print e
+#tests = get_tests()
+
+@tc.parallel()
+def run_test(num):
+    r, info = run_test_function(tests[num])
+    if r in ["pass", "xpass"]:
+        result = (".",  None)
+    elif r == "skipped":
+        result = ("", None)
+    elif r == "xfail":
+        result = ("X", None)
+    else:
+        result = ("F",  info)
+    return result
+
+
+print "running run_test"
+print run_test(range(100))
+
+
+#print run_test(tests[0])
+#print tests[:100]
+#for i in tests[:10]:
+ #   t = run_test(i)
+  #  print i.func_name, t[0], t[1]
+#print run_test(range(100))
+#print "distributing jobs"
+#for target in tasks:
+#    pr = mec.execute("result = run_tests(tests, tasks[%d])" % target,
+#            targets=[target], block=False)
+#    results.append((target, pr))
+#print "collecting results"
+#for proc, r in results:
+#    print "processor:", proc
+#    r.get_result()
+#    result = mec.pull("result", targets=proc)[0]
+#    infos = []
+#    for res, info in result:
+#        if res == ".":
+#            print ".",
+#        else:
+#            print "F",
+#            infos.append(info)
+#    print "exceptions"
+#    for e in infos:
+#        print "_"*80
+#        print e

962ea444a97049f0204ea519d7e7ff619b3bc136
Ondrej Certik <ondrej@certik.cz>
2009-09-12 02:26:27 -0700

    upd

--- sympy/physics/qft.py
+++ sympy/physics/qft.py
@@ -6,7 +6,11 @@ Theory.
 from sympy import factorial
 
 def double_factorial(n):
-    if n in [1, 2, 3]:
+    if n < 0:
+        raise ValueError("n must be positive (got %d)" % n)
+    elif n == 0:
+        return 1
+    elif n in [1, 2, 3]:
         return n
     else:
         return n*double_factorial(n-2)
@@ -21,6 +25,8 @@ def wick(fields):
         n = fields[i]
         if n % 2 == 1:
             return {}
+        elif n == 0:
+            raise ValueError("power should not be 0")
         else:
             return {frozenset(((i, i),)): double_factorial(n-1)}
     elif len(fields) == 2:
@@ -33,17 +39,21 @@ def wick(fields):
             else:
                 import copy
                 d = copy.deepcopy(fields)
-                if fields[i] == 1:
-                    n = d[j]
-                    d[j] -= 1
+                n = d[j]
+                d[i] -= 1
+                d[j] -= 1
+                if d[i] == 0:
                     del d[i]
-                    r = wick(d)
-                    result = {}
-                    for k in r:
-                        k2 = set(k)
-                        k2.add((i, j),)
-                        k2 = frozenset(k2)
-                        result[k2] = r[k]*n
+                if d[j] == 0:
+                    del d[j]
+                r = wick(d)
+                result = {}
+                for k in r:
+                    k2 = set(k)
+                    k2.add((i, j),)
+                    k2 = frozenset(k2)
+                    result[k2] = r[k]*n
+                if fields[i] == 1:
                     return result
                 elif fields[j] == 1:
                     if fields[i] == 3:

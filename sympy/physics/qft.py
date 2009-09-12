9487a97ea65fd7eb53a88fb3f8335adab7cce4eb
Ondrej Certik <ondrej@certik.cz>
2009-09-12 01:37:43 -0700

    wick

---
+++ sympy/physics/qft.py
@@ -0,0 +1,30 @@
+"""
+This module contains utilities for doing calculations in the Quantum Field
+Theory.
+"""
+
+def wick(fields):
+    """
+    fields is a dict of (i -> n) pairs,
+    whose meaning is phi(i)^n
+    """
+    if len(fields) == 1:
+        i = fields.keys()[0]
+        n = fields[i]
+        if n % 2 == 1:
+            return {}
+        else:
+            return {frozenset((1, 1)): n // 2}
+    elif len(fields) == 2:
+        i, j = fields.keys()
+        if (fields[i] + fields[j]) % 2 == 1:
+            return {}
+        else:
+            if fields[i] == fields[j] == 1:
+                return {frozenset((i, j)): 1}
+            else:
+                return {
+                    frozenset(((1, 1), (2, 2))): 1,
+                    frozenset((1, 2)): 2,
+                    }
+
---
+++ t.py
@@ -0,0 +1,23 @@
+from sympy.physics.qft import wick
+
+assert wick({1: 1}) == {}
+assert wick({1: 2}) == {frozenset((1, 1)): 1}
+assert wick({1: 3}) == {}
+assert wick({1: 4}) == {frozenset((1, 1)): 2}
+assert wick({1: 5}) == {}
+assert wick({1: 6}) == {frozenset((1, 1)): 3}
+
+assert wick({1: 1, 2: 1}) == {frozenset((1, 2)): 1}
+assert wick({1: 2, 2: 1}) == {}
+assert wick({1: 1, 2: 2}) == {}
+assert wick({1: 2, 2: 2}) == {
+        frozenset(((1, 1), (2, 2))): 1,
+        frozenset((1, 2)): 2,
+            }
+
+#assert wick({1: 1, 2: 1, 3: 1}) == {}
+#assert wick({1: 1, 2: 1, 3: 1, 4: 1}) == {
+#        frozenset(((1, 2), (3, 4))): 1,
+#        frozenset(((1, 3), (2, 4))): 1,
+#        frozenset(((1, 4), (2, 3))): 1,
+#        }

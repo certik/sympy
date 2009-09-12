78addcbdbf6ccbaf4a55b09f4c9022f4bfec1cde
Ondrej Certik <ondrej@certik.cz>
2009-09-12 01:41:44 -0700

    upd

--- t.py
+++ t.py
@@ -10,10 +10,16 @@ assert wick({1: 6}) == {frozenset((1, 1)): 3}
 assert wick({1: 1, 2: 1}) == {frozenset((1, 2)): 1}
 assert wick({1: 2, 2: 1}) == {}
 assert wick({1: 1, 2: 2}) == {}
+assert wick({1: 1, 2: 3}) == {
+        frozenset(((1, 1), (2, 2))): 1,
+        frozenset((1, 2)): 2,
+            }
 assert wick({1: 2, 2: 2}) == {
         frozenset(((1, 1), (2, 2))): 1,
         frozenset((1, 2)): 2,
             }
+assert wick({1: 3, 2: 2}) == {}
+assert wick({1: 2, 2: 3}) == {}
 
 #assert wick({1: 1, 2: 1, 3: 1}) == {}
 #assert wick({1: 1, 2: 1, 3: 1, 4: 1}) == {

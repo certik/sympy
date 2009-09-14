80f402bc0cd013b9f3ad727600a0712dabc15471
Ondrej Certik <ondrej@certik.cz>
2009-09-13 17:58:39 -0700

    upd

--- t.py
+++ t.py
@@ -7,7 +7,7 @@ print "connected"
 connected = filter_connected(all)
 print "-"*80
 print len(all), len(connected)
-g = {(1, 2): 1, (2, 3): 2, (3, 4): 2, (4, 5): 1, (3, 3): 4}
+g = {(1, 2): 1, (2, 3): 2, (3, 4): 2, (4, 5): 1, (3, 3): 1}
 f = graph_plot(g)
 import pylab
 pylab.show()

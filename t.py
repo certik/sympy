55b28ec9af10d05f3df6d14a912d79b0a6673697
Ondrej Certik <ondrej@certik.cz>
2009-09-20 19:09:57 -0700

    iupd

--- t.py
+++ t.py
@@ -2,12 +2,12 @@ from sympy.physics.qft import (wick, double_factorial, graph2nx, is_connected,
         filter_connected, graph_plot, graph2fields)
 
 print "wick"
-all = wick({1: 1, 2: 1, 3: 1, 4: 1, 5: 4, 6:4})
+all = wick({3:4})
 print "connected"
 connected = filter_connected(all)
 print "-"*80
 print len(all), len(connected)
-for i, g in enumerate(connected):
+for i, g in enumerate(all):
     print g
     f = graph_plot(g)
     f.gca().set_title(str(g))
